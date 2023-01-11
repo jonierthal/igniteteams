babe8de46b64e28820bca09a06a3466c6adfcd82
Jonathan Erthal <jonierthal@gmail.com>
2023-01-11 16:50:23 -0300

    Utilizando fonts

--- App.tsx
+++ App.tsx
@@ -1,12 +1,17 @@
 import { Groups } from '@screens/Groups';
 
+import { ActivityIndicator } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold});
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator/>}
     </ThemeProvider>
   );
 }
