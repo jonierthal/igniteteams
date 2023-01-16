86023b94ad5294cdaa9c9e2a933106ac5cdf9f2e
Jonathan Erthal <jonierthal@gmail.com>
2023-01-16 09:51:04 -0300

    Componente Button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,6 +3,7 @@ import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { useState } from 'react';
 import { FlatList } from 'react-native';
@@ -30,6 +31,10 @@ const [groups, setGroups] = useState<string[]>([])
           )}
           contentContainerStyle={groups.length === 0 && {flex: 1}}
           ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira Turma?" />}
+        />
+
+        <Button 
+          title="Criar nova turma" 
         />
     </Container>
   );
