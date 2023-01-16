38d8995a33e395bb0a6cfbc7a7e0d39839469e05
Jonathan Erthal <jonierthal@gmail.com>
2023-01-16 08:39:15 -0300

    Componente ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,13 +2,15 @@ import { Header } from '@components/Header';
 import { Container } from './styles';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { useState } from 'react';
 import { FlatList } from 'react-native';
 
 
+
 export function Groups() {
-const [groups, setGroups] = useState<string[]>(['Galera da Rocket!'])
+const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -26,6 +28,8 @@ const [groups, setGroups] = useState<string[]>(['Galera da Rocket!'])
               title={item} 
             />
           )}
+          contentContainerStyle={groups.length === 0 && {flex: 1}}
+          ListEmptyComponent={() => <ListEmpty message="Que tal cadastrar a primeira Turma?" />}
         />
     </Container>
   );
