caff0a1d6d9254cc0fa23d292f46b52b13ea8e87
Jonathan Erthal <jonierthal@gmail.com>
2023-01-20 15:56:46 -0300

    Implementando GoBack

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,13 +6,16 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
 import { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 
 import { FlatList } from 'react-native';
 
 
 
-export function Groups({ navigation }) {
+export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
+
+  const navigation = useNavigation();
 
   function handleNewGroup(){
     navigation.navigate('new');
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -4,7 +4,17 @@ import { Highlight } from '@components/Highlight';
 import { Input } from '@components/Input';
 import { Container, Content, Icon } from './styles';
 
+import { useNavigation } from '@react-navigation/native';
+
 export function NewGroup(){
+
+    const navigation = useNavigation();
+
+
+    function handleNew(){
+        navigation.navigate('players', { group: 'Rocket'});
+    }
+
     return (
         <Container>
             <Header showBackButton />
@@ -24,6 +34,7 @@ export function NewGroup(){
                 <Button 
                     title="Criar" 
                     style={{marginTop:20}}
+                    onPress={handleNew}
                 />
             </Content>
         </Container>
