257e766b4a97813fc37529ff7c20b8a3050e4851
Jonathan Erthal <jonierthal@gmail.com>
2023-01-20 11:16:29 -0300

    Navegando pela aplicação

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,12 +6,17 @@ import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
 
 import { useState } from 'react';
+
 import { FlatList } from 'react-native';
 
 
 
-export function Groups() {
-const [groups, setGroups] = useState<string[]>([])
+export function Groups({ navigation }) {
+  const [groups, setGroups] = useState<string[]>([]);
+
+  function handleNewGroup(){
+    navigation.navigate('new');
+  }
 
   return (
     <Container>
@@ -35,6 +40,7 @@ const [groups, setGroups] = useState<string[]>([])
 
         <Button 
           title="Criar nova turma" 
+          onPress={handleNewGroup}
         />
     </Container>
   );
